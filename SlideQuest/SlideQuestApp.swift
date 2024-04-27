a064227ca3abd8ad5f318d65c3a13af52181d805
KDaria <dkdva@mail.ru>
2024-04-27 15:56:12 +0200

    Initial Commit

---
+++ SlideQuest/SlideQuestApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlideQuestApp.swift
+//  SlideQuest
+//
+//  Created by Дарья Кобелева on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct SlideQuestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
